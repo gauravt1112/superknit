b4d6ad342764f181f284cc41a23dc5d80e133c2c
gauravt1112 <33955859+gauravt1112@users.noreply.github.com>
2019-06-16 14:05:20 +0530

    replaced .php to .html

--- contact-us.php
+++ contact-us.php
@@ -4,7 +4,25 @@
 <!--[if IE 8]>         <html class="no-js lt-ie9" lang=""> <![endif]-->
 <!--[if gt IE 8]><!--> <html class="no-js" lang=""> <!--<![endif]-->
     <head>
-        <?php include('includes/includes.html') ?>
+          <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <title>Superknit</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="apple-touch-icon" href="apple-touch-icon.png">
+     <link rel="shortcut icon" type="image/png" href=""/>
+        <link rel="stylesheet" href="css/bootstrap.min.css">
+        <link rel="stylesheet" href="css/bootstrap-theme.min.css">
+        <link rel="stylesheet" href="css/main.css">
+        <link rel="stylesheet" href="css/animate.css">
+    
+  <link rel="stylesheet" type="text/css" href="css/jquery.fancybox.css" media="screen" />
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+      
+        
+ <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.4.0/css/font-awesome.css" rel="stylesheet" media="screen">
+        <script src="js/vendor/modernizr-2.8.3-respond-1.4.2.min.js"></script>
+       
         <style>
 			.contact-active,.contact-active:visited{
 				color: #FFCC33 !important;
@@ -15,7 +33,75 @@
     <div class="contact-us-banner">
     	
     </div>
-      <?php include('includes/header1.html') ?>
+      
+<div class="scroll-up1"> <a href="#top"><img src="img/back-to-top.png" style="width: 70%;"/></a></div>
+
+
+<nav class="navbar navbar-default navbar-static-top" id="nav1" style=" top:0; margin-bottom: 0; width: 100%;">
+  <div class="container-fluid">
+    <!-- Brand and toggle get grouped for better mobile display -->
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="index.php">SUPER KNIT INDUSTRIES</a>
+    </div>
+
+    <!-- Collect the nav links, forms, and other content for toggling -->
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+      <ul class="nav navbar-nav">
+      <a class="navbar-brand1" href="index.php">SUPER KNIT INDUSTRIES</a>
+        <li class="dropdown">
+          <a href="#" class="dropdown-toggle about-active" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">ABOUT US<span class="caret"></span></a>
+          <ul class="dropdown-menu">
+            <li><a href="about-ski.php">ABOUT SKI</a></li>
+            <li><a href="team.php">TEAM</a></li>
+            <li><a href="location.php">LOCATION & INFRASTRUCTURE</a></li>
+             <li><a href="machinery.php">MACHINERY & CAPACITY</a></li>
+             <li><a href="market.php">MARKETS & EXPORTS</a></li>
+             <li><a href="company-culture.php">COMPANY CULTURE</a></li>
+          </ul>
+        </li>
+        
+        <li class="dropdown">
+          <a href="#" class="dropdown-toggle product-active" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">PRODUCTS <span class="caret"></span></a>
+          <ul class="dropdown-menu">
+          <li><a href="product-collection.php">NEW & UNIQUE</a></li>
+            <li><a href="product-men.php">MEN</a></li>
+            <li><a href="product-women.php">WOMEN</a></li>
+            <li><a href="product-kids.php">KIDS</a></li>
+             <li><a href="product-toddlers.php">TODDLERS</a></li>
+             <li><a href="other-product.php">OTHER PRODUCTS</a></li>
+             
+           
+            
+          </ul>
+        </li>
+        
+        <li class="dropdown">
+          <a href="#" class="dropdown-toggle quality-active" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">QUALITY <span class="caret"></span></a>
+          <ul class="dropdown-menu">
+            <li><a href="quality-intro.php">INTRODUCTION</a></li>
+            <li><a href="quality-award.php">AWARD</a></li>
+            <li><a href="raw-material.php">RAW MATERIAL INSPECTION</a></li>
+             <li><a href="quality-check.php">QUALITY CHECKS</a></li>
+           
+            
+          </ul>
+        </li>
+        <li><a href="license.php" class="license-active">LICENSEES & CERTIFICATIONS</a></li>
+         <li><a href="media.php" class="media-active">MEDIA</a></li>
+               <li><a href="contact-us.php" class="contact-active">CONTACT US</a></li>
+               <li><a href="supersox.php" class="supersox-active">SUPERSOX</a></li>
+      </ul>
+    </div><!-- /.navbar-collapse -->
+  </div><!-- /.container-fluid -->
+</nav>
+
+
       
       <div class="contact-us-icon wow fadeIn" data-wow-duration="2s">
       		<img src="img/contact-us-icon.png">
@@ -115,7 +201,95 @@
 </form>
 
     
-      <?php include('includes/footer.html') ?>
+      <!-- footer starts here -->
+
+<style>
+  .size{font-size:15px; color:white; padding: 10px; margin-top: 20px; margin-bottom: 15px;}
+  
+  .size:hover{color: #FFCC33;}
+</style>
+    <footer class="full-length-footer">
+      <div class="footer-icon" style="width: 100%; float: left; text-align: center;">
+        
+        
+        <div class="col-sm-12 col-xs-12 text-center">
+        <a href="https://instagram.com/supersox_india/" target="blank"><img src="img/first-icon.png"></a><a href="https://twitter.com/Supersox_India" target="blank"><img src="img/second-icon.png"></a><a href="https://www.facebook.com/SupersoxIndia/" target="blank"><img src="img/third-icon.png"></a><img src="img/fourth-icon.png"><a href="https://www.youtube.com/channel/UCXAI985uBa5e5OummSvwdXQ" target="blank"><img src="img/fifth-icon.png"></a><a href="https://in.pinterest.com/Supersox_India/" target="blank"><img src="img/sixth-icon.png"></a><img src="img/seventh-icon.png"><!--<i class="fa fa-instagram size"></i><i class="fa fa-twitter size"></i><i class="fa fa-facebook size"></i><i class="fa fa-google-plus size"></i><i class="fa fa-youtube-play size"></i><i class="fa fa-pinterest-p size"></i><img src="img/quora-icon.png" style="width: 35px;">-->
+      </div>      
+      </div>
+      
+      
+      
+      
+      <div class="footer-links" style="width:100%;">
+      
+        <a href="about-ski.php" class="about-active">About us</a> <a href="product-men.php" class="product-active">Products</a><a href="quality-intro.php" class="quality-active">Quality</a><a href="license.php" class="license-active">Licensees & Certifications</a><a href="media.php" class="media-active">Media</a><a href="careers.php" class="career-active">Careers</a><a href="process.php" class="process-active">Process</a><a href="contact-us.php" class="contact-active">Contact Us</a><a href="supersox.php" class="supersox-active">Supersox</a>
+        
+      </div>
+      
+      <div class="footer-text">
+      
+        © Copyright 2015 Super Knit Industries. All Rights Reserved.
+        
+      </div>
+      
+      
+    </footer>
+    
+  
+  
+  
+  
+  <footer class="container-fluid visible-xs visible-sm">
+  
+    <div class="row">
+      <div class="col-sm-12 col-xs-12 footer-icon">
+        <a href="https://instagram.com/supersox_india/" target="blank"><img src="img/first-icon.png"></a><a href="https://twitter.com/Supersox_India" target="blank"><img src="img/second-icon.png"></a><a href="https://www.facebook.com/SupersoxIndia/" target="blank"><img src="img/third-icon.png"></a><img src="img/fourth-icon.png"><a href="https://www.youtube.com/channel/UCXAI985uBa5e5OummSvwdXQ" target="blank"><img src="img/fifth-icon.png"></a><a href="https://in.pinterest.com/Supersox_India/" target="blank"><img src="img/sixth-icon.png"></a><img src="img/seventh-icon.png"><!--<i class="fa fa-instagram size"></i><i class="fa fa-twitter size"></i><i class="fa fa-facebook size"></i><i class="fa fa-google-plus size"></i><i class="fa fa-youtube-play size"></i><i class="fa fa-pinterest-p size"></i><img src="img/quora-icon.png" style="width: 35px;">-->
+      </div>
+    </div>
+  
+    <div class="row text-left visible-sm" style="width: 80%; margin: 0 auto;">
+      <div class="col-sm-4">
+          <a href="about-ski.php" class="about-active">About us</a><br>
+          <a href="product-men.php" class="product-active">Products</a><br>
+          <a href="quality-intro.php" class="quality-active">Quality</a>
+      </div>
+      <div class="col-sm-4">
+        <a href="license.php" class="license-active">Licensees & Certifications</a><br>
+        <a href="media.php" class="media-active">Media</a><br>
+        <a href="process.php" class="process-active">Process</a>
+      </div>
+      <div class="col-sm-4">
+        <a href="contact-us.php" class="contact-active">Contact Us</a><br>
+        <a href="supersox.php" class="supersox-active">Supersox</a><br>
+        <a href="careers.php" class="career-active">Careers</a>
+      </div>
+    </div> 
+    
+    
+    <div class="row text-left visible-xs" style="width: 80%; margin: 0 auto;">
+      <div class="col-xs-6">
+          <a href="about-ski.php" class="about-active">About us</a><br>
+          <a href="product-men.php" class="product-active">Products</a><br>
+          <a href="quality-intro.php" class="quality-active">Quality</a><br>
+          <a href="license.php" style="display: inline-block;" class="license-active">Licensees & Certifications</a><br>
+          <a href="careers.php" class="career-active">Careers</a>
+      </div>
+      <div class="col-xs-5 col-xs-offset-1">
+        <a href="media.php" class="media-active">Media</a><br>
+        <a href="process.php" class="process-active">Process</a><br>
+        <a href="contact-us.php" class="contact-active">Contact Us</a><br>
+        <a href="supersox.php" class="supersox-active">Supersox</a><br>
+      </div>
+    </div>
+    
+    
+    <div class="row">
+      <div class="col-sm12 col-xs-12 footer-text" style="margin-top: 15px;">
+        &copy; Copyright 2015 Super Knit Industries. All Rights Reserved.
+      </div>
+    </div>
+  </footer>
+    
     
        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
        
